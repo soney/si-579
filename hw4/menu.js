eb671364763605831794c3f7492b51ba4466cceb
April <aprilww@umich.edu>
2021-03-01 11:58:34 -0500

    🔥 remove locals

--- hw4/menu.js
+++ hw4/menu.js
@@ -1,34 +1,32 @@
 const hideSpicyItem = () => {
-    const spicyItems = document.getElementsByClassName('spicy');
-    for(item of spicyItems) {
-        item.style.display = "none";
-    }
-}
+	const spicyItems = document.getElementsByClassName("spicy");
+	for (item of spicyItems) {
+		item.style.display = "none";
+	}
+};
 
 const displayAll = () => {
-    const spicyItems = document.getElementsByClassName('spicy');
-    for(item of spicyItems) {
-        item.style.display = "block";
-    }
-}
+	const spicyItems = document.getElementsByClassName("spicy");
+	for (item of spicyItems) {
+		item.style.display = "block";
+	}
+};
 
 const addItem = () => {
-    const itemName = document.getElementById('item-name').value;
-    const isSpicy = document.getElementById('is-spicy').checked;
-    const lunchList = document.getElementById('lunch-list');
-    const newItem = document.createElement('li');
-    newItem.className = isSpicy? "spicy":"";
-    newItem.textContent = itemName;
-    lunchList.appendChild(newItem);
-}
+	const itemName = document.getElementById("item-name").value;
+	const isSpicy = document.getElementById("is-spicy").checked;
+	const lunchList = document.getElementById("lunch-list");
+	const newItem = document.createElement("li");
+	newItem.className = isSpicy ? "spicy" : "";
+	newItem.textContent = itemName;
+	lunchList.appendChild(newItem);
+};
 
+const hide_spicy_button = document.getElementById("hide-spicy-button");
+hide_spicy_button.addEventListener("click", hideSpicyItem);
 
+const all_button = document.getElementById("all-button");
+all_button.addEventListener("click", displayAll);
 
-const hide_spicy_button = document.getElementById('hide-spicy-button');
-hide_spicy_button.addEventListener('click', hideSpicyItem);
-
-const all_button = document.getElementById('all-button');
-all_button.addEventListener('click', displayAll);
-
-const add_button = document.getElementById('add-button');
-add_button.addEventListener('click', addItem);
+const add_button = document.getElementById("add-button");
+add_button.addEventListener("click", addItem);
